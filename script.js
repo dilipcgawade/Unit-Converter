df177083733dccef6b89250281a715969765aa56
Dilip Gawade <dilipcgawade.com>
2023-06-22 16:58:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let feet = document.getElementById('feet');
+let inch = document.getElementById('inch');
+
+feet.addEventListener('input', function(){
+    let f = this.value;
+    let i = f*12;
+    inch.value = i;
+});
+inch.addEventListener('input', function(){
+    let i = this.value;
+    let f = i/12;
+    // If you want to show only two decimals after point,
+    if(!Number.isInteger(f)){
+        f = f.toFixed(2);
+    };
+    feet.value = f;
+});
